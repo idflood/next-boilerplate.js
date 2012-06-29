c18d6b24631c02bb0527ba0cf4fcac7e0a6ce509
David Mignot <david.mig@gmail.com>
2012-06-29 08:08:13 +0200

    updated to requirejs 2

--- src/scripts/require-config.js
+++ src/scripts/require-config.js
@@ -7,7 +7,16 @@ require.config({
     text: "libs/require/text",
     "socket.io": "libs/socket.io/socket.io",
     cs: "libs/require/cs",
-    CoffeeScript: "libs/coffee-script"
+    CoffeeScript: "libs/coffee-script",
+    Three: "libs/three.js/build/Three",
+    EffectComposer: 'libs/three.js/examples/js/postprocessing/EffectComposer',
+    RenderPass: 'libs/three.js/examples/js/postprocessing/RenderPass',
+    BloomPass: 'libs/three.js/examples/js/postprocessing/BloomPass',
+    FilmPass: 'libs/three.js/examples/js/postprocessing/FilmPass',
+    TexturePass: 'libs/three.js/examples/js/postprocessing/TexturePass',
+    ShaderPass: 'libs/three.js/examples/js/postprocessing/ShaderPass',
+    MaskPass: 'libs/three.js/examples/js/postprocessing/MaskPass',
+    ShaderExtras: 'libs/three.js/examples/js/ShaderExtras'
   },
   shim: {
     'Underscore': {
@@ -20,6 +29,17 @@ require.config({
     'jquery.ui': {
       deps: ['jquery'],
       exports: "jquery"
-    }
+    },
+    'Three': {
+      exports: "THREE"
+    },
+    'EffectComposer': ['Three'],
+    'RenderPass': ['Three'],
+    'BloomPass': ['Three'],
+    'FilmPass': ['Three'],
+    'TexturePass': ['Three'],
+    'ShaderPass': ['Three'],
+    'MaskPass': ['Three'],
+    'ShaderExtras': ['Three']
   }
 });
